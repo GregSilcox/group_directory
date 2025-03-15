0659099a3dbbbb6c489d802d8cf0d5e6da1da48b
Greg Silcox <greg.silcox@gmail.com>
2025-03-15 14:51:01 -0700

    Completed all the request specs for users and phones

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,16 +10,128 @@ RSpec.describe "/users", type: :request do
   }
 
   describe "GET /index" do
+    it "renders a successful response" do
+      User.create! valid_attributes
+      get users_url
+      expect(response).to be_successful
+    end
   end
 
   describe "GET /show" do
+    it "renders a successful response" do
+      user = User.create! valid_attributes
+      get user_url(user)
+      expect(response).to be_successful
+    end
   end
 
   describe "GET /new" do
     it "renders a successful response" do
       get new_user_url
-      Rails.logger.info "Response: #{ response.body }" # This will log the text to the Rails log
-      expect(response.body).to include("l User")
+      expect(response).to be_successful
+    end
+
+    it "renders the correct form" do
+      get new_user_url
+      expect(response.body).to include("New User")
+    end
+  end
+
+  describe "GET /edit" do
+    it "renders a successful response" do
+      user = User.create! valid_attributes
+      get edit_user_url(user)
+      expect(response).to be_successful
+    end
+
+    it "renders the correct form" do
+      user = User.create! valid_attributes
+      get edit_user_url(user)
+      expect(response.body).to include("Editing User")
+    end
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      it "creates a new User" do
+        expect {
+          post users_url, params: { user: valid_attributes }
+        }.to change(User, :count).by(1)
+      end
+
+      it "redirects to the created user" do
+        post users_url, params: { user: valid_attributes }
+        expect(response).to redirect_to(user_url(User.last))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new User" do
+        expect {
+          post users_url, params: { user: invalid_attributes }
+        }.to change(User, :count).by(0)
+      end
+
+      it "renders a successful response (i.e. to display the 'new' template)" do
+        post users_url, params: { user: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "renders the correct form" do
+        post users_url, params: { user: invalid_attributes }
+        expect(response.body).to include("New User")
+      end
+    end
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) {
+        { email_address: "test@example.com", password: "password", password_confirmation: "password" }
+      }
+
+      it "updates the requested user" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: new_attributes }
+        user.reload
+        expect(user.email_address).to eq("test@example.com")
+      end
+
+      it "redirects to the user" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: new_attributes }
+        user.reload
+        expect(response).to redirect_to(user_url(user))
+      end
+    end
+
+    context "with invalid parameters" do
+      it "renders a response with 422 status (i.e. to display the 'edit' template)" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "renders the correct form" do
+        user = User.create! valid_attributes
+        patch user_url(user), params: { user: invalid_attributes }
+        expect(response.body).to include("Editing User")
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "destroys the requested user" do
+      user = User.create! valid_attributes
+      expect {
+        delete user_url(user)
+      }.to change(User, :count).by(-1)
+    end
+
+    it "redirects to the users list" do
+      user = User.create! valid_attributes
+      delete user_url(user)
+      expect(response).to redirect_to(users_url)
     end
   end
 end
